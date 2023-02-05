77babd06fe2ae0703a36a589ca164009fefb9bd0
Kcbehrens <kcbehrens@my365.bellevue.edu>
2023-02-04 23:42:40 -0600

    Update pysports_queries.py

--- module_8/pysports_queries.py
+++ module_8/pysports_queries.py
@@ -6,6 +6,7 @@
 #Connects MySQL with Python
 import mysql.connector
 from mysql.connector import errorcode
+import time
 
 #Configuration data for database
 config = {
@@ -19,18 +20,36 @@ config = {
 #Shows whether connection is successful or not given the input
 try:
     db = mysql.connector.connect(**config)
-    print("\nDatabase user " + format(config["user"]) + " connected to MySQL on host configuration " + config["host"] + " with database " + config["database"] + ".")
-    #Selecting the query
-    SELECT team_id, team_name, mascot FROM team;
 
     #Cursor
     cursor = db.cursor()
-    cursor.execute(“SELECT team_id, team_name, mascot FROM team”)
+    
+    #Select the query
+    cursor.execute("SELECT team_id, team_name, mascot FROM team")
+    
+    #Sleep timer for 2 seconds to gather data for output
+    time.sleep(2)
+    
+    #Results from the query
     teams = cursor.fetchall()
-
+    print("-- DISPLAYING TEAM RECORDS --")
+    
+    #Gather results from teams
     for team in teams:
-        print("Team Name: {}".format(team[1]))
-        input("\n\nPress any key to continue...")
+        print("Team ID: {}\nTeam Name: {}\nMascot: {}\n".format(team[0], team[1], team[2]))
+    
+    #Select player table information
+    cursor.execute("SELECT player_id, first_name, last_name, team_id FROM player")
+    
+    #Results from the query
+    players = cursor.fetchall()
+    print ("\n-- DISPLAYING PLAYER RECORDS --")
+
+    #Gather the results of the players
+    for player in players:
+        print("Player ID: {}\nFirst Name: {}\nLast Name: {}\nTeam ID: {}\n".format(player[0], player[1], player[2], player[3]))
+
+    input("\nPress any key to continue... ")
 
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
